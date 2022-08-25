b180fcd37dccaffbab25262a51447b462253e05b
sohan <sohanagashimanii@gmail.com>
2022-08-25 21:50:15 +0530

    fixed warning

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,5 +4,5 @@ import chatApp from "../images/chatApp.jpg";
 import notesApp from "../images/notes-app.jpg";
 import textUtils from "../images/textUtils.jpg";
 import movie from "../images/movie.jpg";
-
+// eslint-disable-next-line
 export default { newsapp, leave, chatApp, notesApp, textUtils, movie };
